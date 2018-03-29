15d754c9e778a24311f1ab63b33f92a9015d85cd
jakedv24 <jakedveatch@gmail.com>
2018-03-29 13:49:07 -0500

    Create TODO.java

---
+++ 3 - Conditional Statements/TODO.java
@@ -0,0 +1,56 @@
+package conditionalStatements;
+
+public class Jake {
+	
+	/**
+	 * Determines if string a and b are the same word
+	 * @param a
+	 * 	first word
+	 * @param b
+	 * 	second word
+	 * @return
+	 * 	True if the words are the same, false otherwise
+	 */
+	public static boolean areSameWord(String a, String b) {
+		//TODO
+		return false;
+	}
+	
+	/**
+	 * Determines if the string s is in all caps
+	 * @param s
+	 * 	Given string
+	 * @return
+	 * 	True if all caps, false otherwise
+	 */
+	public static boolean isCapsLocked(String s) {
+		//TODO
+		return false;
+	}
+	
+	/**
+	 * Determines if the first value in each array are the same.
+	 * @param a
+	 * 	First int array
+	 * @param b
+	 * 	Second int array
+	 * @return
+	 * 	True if a and b have the same first value
+	 */
+	public static boolean startSameNumber(int[] a, int[] b) {
+		//TODO
+		return false;
+	}
+	
+	/**
+	 * Determines if there is a 6 in the array, followed by a nine in the next position.
+	 * @param arr
+	 * 	The array searching for [...,6,9,...]
+	 * @return
+	 * 	True if 6 then 9 is found, false otherwise
+	 */
+	public static int findSixtyNine(int[] arr) {
+		//TODO
+		return 0;
+	}
+}
